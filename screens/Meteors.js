63133d191b955e4a8d6d3d6938498cb6fe5a30e2
jinie-singh <74295187+jinie-singh@users.noreply.github.com>
2022-01-17 15:21:28 +0000

    Add files via upload

---
+++ screens/Meteors.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Text, View, Alert } from 'react-native';
+import axios from "axios";
+
+export default class MeteorScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            meteors: {},
+        };
+    }
+
+    componentDidMount() {
+        this.getMeteors()
+    }
+
+    getMeteors = () => {
+        axios
+            .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=sEs1DsBJJgYsZ5VeCUUgIgkrt6Yqo2NL4UfOBLi9")
+            .then(response => {
+                this.setState({ meteors: response.data.near_earth_objects })
+            })
+            .catch(error => {
+                Alert.alert(error.message)
+            })
+    }
+
+    render() {
+        if (Object.keys(this.state.meteors).length === 0) {
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                    <Text>Loading</Text>
+                </View>
+            )
+        } else {
+            var meteor_arr = Object.keys(this.state.meteors).map(meteor_date => {
+                return this.state.meteors[meteor_date]
+            })
+            var meteors = [].concat.apply([], meteor_arr);
+
+            
+
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                    <Text>Meteor Screen!</Text>
+                </View>
+            )
+        }
+    }
+}
